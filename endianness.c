74b8efb54e1d04e5cb3d7d04ed5823be207df889
Gabriel Clima <gabriel.clima@gcore.com>
2023-08-09 12:13:22 +0300

    endianness test

---
+++ endianness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+union test {
+	int i;
+	char c[sizeof(int)];
+};
+
+int main() {
+
+	union test t;
+	t.i = 1;
+
+	if (t.c[0] & 1) {
+		printf("Little endian\n");
+	} else {
+		printf("Big endian\n");
+	}
+}
